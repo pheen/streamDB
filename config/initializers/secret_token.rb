97af534a8fbbb352398e6bc066a191f8759a510b
Joel Korpela <syright@gmail.com>
2014-01-25 21:21:01 -0700

    Default Rails 4.0.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StreamDB::Application.config.secret_key_base = '628a4f45df6ffa65c352f531d1a6c70a20ef326de8b57532e72f41e1ce30bfd53c893dbc450ac206b0c8f3bfc2c136bcf9965e3cea7ba7f57c298bb7b4b0d64e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StreamDB::Application.config.session_store :cookie_store, key: '_streamDB_session'
