2bb16adf154c0d3bfbbdab7ac7f1a6e9996f72f3
Joel Korpela <syright@gmail.com>
2014-04-13 13:55:41 -0600

    worker fixes

--- workers/update_streams.rb
+++ workers/update_streams.rb
@@ -1,4 +1,15 @@
-require 'active_support/core_ext'
+require 'active_record'
+require 'pg'
+require 'models/video'
 require 'robot'
+
+def setup_database
+  puts "Database connection details:#{params['database'].inspect}"
+  return unless params['database']
+  # estabilsh database connection
+  ActiveRecord::Base.establish_connection(params['database'])
+end
+
+setup_database
 
 Robot.scrape(params['sports'])
