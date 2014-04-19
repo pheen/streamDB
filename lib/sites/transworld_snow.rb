1ae4f36d135576a906b94a9c1b47ee508692911c
Joel Korpela <syright@gmail.com>
2014-04-18 19:45:48 -0600

    .

--- lib/sites/transworld_snow.rb
+++ lib/sites/transworld_snow.rb
@@ -1,3 +1,5 @@
+require 'nokogiri'
+
 module Sites
   class TransworldSnow
     include Wordpress
--- lib/wordpress.rb
+++ lib/wordpress.rb
@@ -1,3 +1,5 @@
+require 'nokogiri'
+
 module Wordpress
   def video_nodes(url)
     xml = Nokogiri::XML(open(url))
